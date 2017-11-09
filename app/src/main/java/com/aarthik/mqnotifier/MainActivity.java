d3fa154f0d1fdd76b8f1d66b590d67a83a9c4500
Aarthik Rao <aaarthik@gmail.com>
2017-11-09 21:43:56 +0530

    Added RabbitMQ

--- app/src/main/java/com/aarthik/mqnotifier/MainActivity.java
+++ app/src/main/java/com/aarthik/mqnotifier/MainActivity.java
@@ -1,13 +1,51 @@
 package com.aarthik.mqnotifier;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String QUEUE_NAME = "hello";
+    Context context = getApplicationContext();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try {
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+            Consumer consumer = new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                    String message = new String(body, "UTF-8");
+                    Toast.makeText(context,message,Toast.LENGTH_LONG);
+                }
+            };
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } {
+
+        }
     }
+
+
 }
