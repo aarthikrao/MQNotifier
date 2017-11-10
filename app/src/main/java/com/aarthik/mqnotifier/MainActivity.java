5fb9421719ecd2792688cde0f6917ebb564a3a8f
Aarthik Rao <aaarthik@gmail.com>
2017-11-10 21:37:32 +0530

    Rabbit MQ updated

--- app/src/main/java/com/aarthik/mqnotifier/MainActivity.java
+++ app/src/main/java/com/aarthik/mqnotifier/MainActivity.java
@@ -2,7 +2,9 @@ package com.aarthik.mqnotifier;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.rabbitmq.client.AMQP;
@@ -18,24 +20,30 @@ import java.util.concurrent.TimeoutException;
 
 public class MainActivity extends AppCompatActivity {
     private final static String QUEUE_NAME = "hello";
-    Context context = getApplicationContext();
+///    Context context = getApplicationContext();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
+        factory.setHost("192.168.1.7");
+        factory.setPort(5672);
+        factory.setUsername("admin");
+        factory.setPassword("admin");
         try {
             Connection connection = factory.newConnection();
             Channel channel = connection.createChannel();
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-
+            channel.exchangeDeclare("logs","fanout");
             Consumer consumer = new DefaultConsumer(channel) {
                 @Override
                 public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                     String message = new String(body, "UTF-8");
-                    Toast.makeText(context,message,Toast.LENGTH_LONG);
+//                    Toast.makeText(context,message,Toast.LENGTH_LONG);
+                    Log.i("",message);
                 }
             };
         } catch (TimeoutException e) {
