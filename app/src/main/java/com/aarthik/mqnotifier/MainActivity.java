9ea02b56b3027bbdb395568aa26028d04175d946
Aarthik Rao <aaarthik@gmail.com>
2017-11-13 21:48:49 +0530

    Basic working app

--- app/src/main/java/com/aarthik/mqnotifier/MainActivity.java
+++ app/src/main/java/com/aarthik/mqnotifier/MainActivity.java
@@ -1,11 +1,16 @@
 package com.aarthik.mqnotifier;
 
+import android.app.NotificationManager;
 import android.content.Context;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
@@ -28,32 +33,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
+        final TextView DisplayText = (TextView) findViewById(R.id.displayText);
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("192.168.1.7");
         factory.setPort(5672);
         factory.setUsername("admin");
         factory.setPassword("admin");
+        factory.setAutomaticRecoveryEnabled(true);
         try {
             Connection connection = factory.newConnection();
             Channel channel = connection.createChannel();
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-            channel.exchangeDeclare("logs","fanout");
+//            channel.exchangeDeclare("logs","fanout");
+            Log.i("", "Channel declared");
             Consumer consumer = new DefaultConsumer(channel) {
                 @Override
                 public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
                     String message = new String(body, "UTF-8");
-//                    Toast.makeText(context,message,Toast.LENGTH_LONG);
-                    Log.i("",message);
+//                    Toast.makeText(context,message, Toast.LENGTH_LONG);
+                    Log.i(message, message);
+                    setText(DisplayText, message);
+                    Log.i("", "In the method");
                 }
             };
+            channel.basicConsume(QUEUE_NAME, true, consumer);
+
         } catch (TimeoutException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        } {
+        }
+        {
 
         }
     }
 
+    private void setText(final TextView text, final String value) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                text.setText(value);
+                sendNotification("Hello", value);
+            }
+        });
+    }
 
+    public void sendNotification(String title, String text) {
+        Uri uri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(getApplicationContext())
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle(title)
+                        .setContentText(text).setVibrate(new long[]{500, 500, 500, 500, 500})
+                        .setLights(Color.RED, 500, 500)
+                        .setSound(uri);
+
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        synchronized (mNotificationManager) {
+            mNotificationManager.notify(1, mBuilder.build());
+        }
+    }
 }
+
